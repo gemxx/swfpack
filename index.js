1bc5c9a844b6757a1de1e659e73100f8c9ea4a9c
gemxx <gems.xu@gmail.com>
2017-05-05 20:01:09 +0800

    refactor lzma construct.

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ Swf.prototype = {
     /**
      * parse swf.
      */
-    _parse: function() {
+    _parse: function () {
         var swf_buf = this.buffer;
         var signature = '';
 
@@ -57,26 +57,26 @@ Swf.prototype = {
         }
 
         switch (strfcc(swf_buf[0])) {
-        case 'F':
-            // swf uncompressed.
-            signature = 0;
+            case 'F':
+                // swf uncompressed.
+                signature = 0;
 
-            this.uncompressed = swf_buf.slice(8);
-            break;
-        case 'C':
-            // swf compressed by zlib.
-            signature = 1;
+                this.uncompressed = swf_buf.slice(8);
+                break;
+            case 'C':
+                // swf compressed by zlib.
+                signature = 1;
 
-            this.uncompressed = zlib.inflateSync(swf_buf.slice(8));
-            break;
-        case 'Z':
-            // swf compressed by lzma.
-            signature = 2;
+                this.uncompressed = zlib.inflateSync(swf_buf.slice(8));
+                break;
+            case 'Z':
+                // swf compressed by lzma.
+                signature = 2;
 
-            this.uncompressed = lzma.decompress(swf_buf.slice(12));
-            break;
-        default:
-            exit('not a swf file!');
+                this.uncompressed = lzma.decompress(swf_buf.slice(12));
+                break;
+            default:
+                exit('not a swf file!');
         }
 
         if (strfcc(swf_buf[1]) !== 'W' && strfcc(swf_buf[2]) !== 'S') {
@@ -89,12 +89,12 @@ Swf.prototype = {
         this.fwsBuffer = Buffer.concat([this.buffer.slice(0, 8), this.uncompressed]); // FWS Buffer.
 
     },
-    _check: function() {
+    _check: function () {
         if (this.fwsBuffer.length !== this.filelength) {
             exit('swf file length check failed!');
         }
     },
-    pack: function(mode, level) {
+    pack: function (mode, level) {
         var swf_formats = {
             'fws': 0,
             'cws': 1,
@@ -110,25 +110,41 @@ Swf.prototype = {
         headers.writeUInt32LE(this.fwsBuffer.length, 4);
 
         switch (sign) {
-        case 0:
-            headers[0] = 'F'.charCodeAt(0);
-            output = Buffer.concat([headers, this.fwsBuffer.slice(8)]);
-            break;
-        case 1:
-            headers[0] = 'C'.charCodeAt(0);
-            output = Buffer.concat([headers, zlib.deflateSync(this.uncompressed, {finishFlush: zlib.Z_SYNC_FLUSH})]);
-            break;
-        case 2:
-            headers = this.fwsBuffer.slice(0, 12); // copy header.
-            headers[0] = 'Z'.charCodeAt(0);
-            headers[3] = headers[3] > 13 ? headers[3] : 13;
-            headers.writeUInt32LE(this.fwsBuffer.length + 8, 4);
+            case 0:
+                headers[0] = 'F'.charCodeAt(0);
+                output = Buffer.concat([headers, this.fwsBuffer.slice(8)]);
+                break;
+            case 1:
+                headers[0] = 'C'.charCodeAt(0);
+                output = Buffer.concat([headers, zlib.deflateSync(this.uncompressed, {finishFlush: zlib.Z_SYNC_FLUSH})]);
+                break;
+            case 2:
+                /**
+                 * Format of LZMA SWF
+                 *
+                 * +-----------------+-----------+---------------+------------+-----------+-----------------+
+                 * | 4 bytes         | 4 bytes   | 4 bytes       | 5 bytes    | n bytes   | 6 bytes         |
+                 * +-----------------+-----------+---------------+------------+-----------+-----------------+
+                 * | 'ZWS' + version | scriptLen | compressedLen | LZMA props | LZMA data | LZMA end marker |
+                 * +-----------------+-----------+---------------+------------+-----------+-----------------+
+                 *
+                 * scriptLen is the uncompressed length of the SWF data. Includes 4 bytes
+                 * SWF header and 4 bytes for scriptLen itself.
+                 *
+                 * compressedLen does not include header (4+4+4 bytes) or lzma props (5
+                 * bytes) compressedLen does include LZMA end marker (6 bytes);
+                 *
+                 */
+                headers = this.fwsBuffer.slice(0, 12); // copy header.
+                headers[0] = 'Z'.charCodeAt(0);
+                headers[3] = headers[3] > 13 ? headers[3] : 13;
+                headers.writeUInt32LE(this.fwsBuffer.length + 8, 4);
 
-            var lzmaBuffer = new Buffer(lzma.compress(this.uncompressed, this.lzma_mode));
-            headers.writeUInt32LE(lzmaBuffer.length - 13, 8);
+                var lzmaBuffer = new Buffer(lzma.compress(this.uncompressed, this.lzma_mode));
+                headers.writeUInt32LE(lzmaBuffer.length - 13, 8);
 
-            output = Buffer.concat([headers, lzmaBuffer.slice(0, 5), lzmaBuffer.slice(13)]);
-            break;
+                output = Buffer.concat([headers, lzmaBuffer.slice(0, 5), lzmaBuffer.slice(13)]);
+                break;
         }
 
         return output;
